94f928fdee1ffd85472d5a72bdba3a4f74fc3060
fpt-phongnx8 <phongnx8@fpt.com>
2023-11-13 17:21:39 +0900

    change URL

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a05937e42b7974f6ba3d6e942ebc632b-977522545.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
