b9d613a77063469a3a651ab4bb38bb2e095db3a1
fpt-phongnx8 <phongnx8@fpt.com>
2023-11-14 14:50:26 +0900

    Add Screenshot

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=MyPassword1!
+export POSTGRES_HOST=postgres-db.cu6ps2y9oemp.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::udacity-microservice-user-upload-pictures
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=micro
 export JWT_SECRET=testing
 export URL=http://localhost:8100
